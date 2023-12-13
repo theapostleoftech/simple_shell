d1c4e0cd2cb2998b20337751a14f9ededed7ec53
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-12-12 22:20:57 -0800

    util updated

--- util.c
+++ util.c
@@ -6,62 +6,43 @@
  * 
  * Return: split string
  */
-char **string_splitter(char *string, const char* separator)
+char **string_splitter(char *string, const char *separator)
 {
-    static char *buffer;
-    char *tkn;
-    char **tkns;
-    size_t i = 0;
-
-    if (string != NULL)
-    {
-        buffer = string;
-    }
-
-    if (buffer == NULL)
-    {
-        return (NULL);
-    }
-    tkns = malloc(sizeof(char *));
-
-    if (!tkns)
-    {
-        return (NULL);
-    }
-
-    while ((tkn = strtok(buffer, separator)) != NULL)
-    {
-        buffer = NULL;
-
-        tkns[i] = strdup(tkn);
-
-        if (!tkns[i])
-        {
-            while (i > 0)
-            {
-                free(tkns[--i]);
-            }
-            free(tkns);
-            return (NULL);
-        }
-
-        i++;
-        tkns = realloc(tkns, (i + 1) * sizeof(char *));
-
-        if (!tkns)
-        {
-            while (i > 0)
-            {
-                free(tkns[--i]);
-            }
-            free(tkns);
-            return (NULL);
-        }
-    }
-
-    tkns[i] = NULL;
-
-    return (tkns);
+	static char *buffer;
+	char *tkn, char **tkns;
+	size_t i = 0;
+	
+	if (string != NULL)
+		buffer = string;
+	if (buffer == NULL)
+		return (NULL);
+	tkns = malloc(sizeof(char *));
+	if (!tkns)
+		return (NULL);
+			
+			while ((tkn = strtok(buffer, separator)) != NULL)
+			{
+				buffer = NULL;
+				tkns[i] = strdup(tkn);
+				if (!tkns[i])
+				{
+					while (i > 0)
+						free(tkns[--i]);
+					free(tkns);
+					return (NULL);
+				}
+				i++;
+				tkns = realloc(tkns, (i + 1) * sizeof(char *));
+				if (!tkns)
+				{
+					while (i > 0)
+						free(tkns[--i]);
+					free(tkns);
+					return (NULL);
+				}
+			}
+	tkns[i] = NULL;
+	return (tkns);
 }
 
 
@@ -74,13 +55,12 @@ char **string_splitter(char *string, const char* separator)
  */
 int _strcompare(const char *s1, const char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
 /**
@@ -91,23 +71,23 @@ int _strcompare(const char *s1, const char *s2)
  */
 int _atoint(char *string)
 {
-    int output = 0, symb = 1;
-
-    while (*string == ' ' || (*string >= '\t' && *string <= 'r'))
-    {
-        string++;
-    }
-
-    if (*string == '-' || *string == '+')
-    {
-        symb = (*string == '-') ? -1 : 1;
-        string++;
-    }
-    while (*string >= '0' && *string <= '9')
-    {
-        output = output * 10 + (*string - '0');
-        string++;
-    }
-    return (output * symb);
+	int output = 0, symb = 1;
+	
+	while (*string == ' ' || (*string >= '\t' && *string <= 'r'))
+	{
+		string++;
+	}
+	if (*string == '-' || *string == '+')
+	{
+		symb = (*string == '-') ? -1 : 1;
+		string++;
+	}
+	
+	while (*string >= '0' && *string <= '9')
+	{
+		output = output * 10 + (*string - '0');
+		string++;
+	}
+	return (output * symb);
 }
 
