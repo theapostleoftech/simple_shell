1f39b9f359390a937831c63ff8ae2464ec317bfd
Blessdeb <oyemadedeborah@gmail.com>
2023-12-12 17:49:32 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -3,51 +3,53 @@
  * cmdline_arguments - handle cmdline arguments
  * @arc: argument count
  * @argv: argument vector
- * 
+ *
  * Return: void
  */
 void cmdline_arguments(int argc, char *argv[])
 {
-    int i;
+int i;
 
-    if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            execute_command(argv[i]);
-        }
+if (argc > 1)
+	{
+	for (i = 1; i < argc; i++)
+	{
+	execute_command(argv[i]);
+	}
 
-    }
-    else
-    {
-        char *line;
-        while(1)
-        {
-            write(1, SHELL_SIGN, strlen(SHELL_SIGN));
-            line = my_getline();
-            if (line == NULL)
-            {
-                free(line);
-                continue;
-            }
-            execute_command(line);
-            free(line);
-        }
-    }
+	}
+	else
+	{
+	char *line;
+
+	while (1)
+
+	{
+		write(1, SHELL_SIGN, strlen(SHELL_SIGN));
+		line = my_getline();
+if (line == NULL)
+	 {
+		free(line);
+		continue;
+	}
+		execute_command(line);
+		free(line);
+	}
+	}
 
 }
 
 
 
 /**
- * @brief 
- * 
- * @return int 
+ * @main
+ *
+ * @return int
  */
 int main(int argc, char *argv[])
 {
-    cmdline_arguments(argc, argv);
+	cmdline_arguments(argc, argv);
 
-    return (0);
+	return (0);
 
 }
