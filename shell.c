9db3e7bd18b96b5da074fb9aedf87072f34c6fb9
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-12-12 21:52:01 -0800

    Files updated

--- shell.c
+++ shell.c
@@ -1,53 +1,53 @@
 #include "shell.h"
 /**
  * cmdline_arguments - handle cmdline arguments
- * @arc: argument count
- * @argv: argument vector
- * 
+ * @argc: argument count.
+ * @argv: args vector.
+ *
  * Return: void
  */
 void cmdline_arguments(int argc, char *argv[])
 {
-    int i;
+	int i;
 
-    if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            execute_command(argv[i]);
-        }
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			execute_command(argv[i]);
+		}
+	}
+	else
+	{
+		char *line;
 
-    }
-    else
-    {
-        char *line;
-        while(1)
-        {
-            write(1, SHELL_SIGN, strlen(SHELL_SIGN));
-            line = my_getline();
-            if (line == NULL)
-            {
-                free(line);
-                continue;
-            }
-            execute_command(line);
-            free(line);
-        }
-    }
-
+		while (1)
+		{
+			write(1, SHELL_SIGN, strlen(SHELL_SIGN));
+			line = my_getline();
+			if (line == NULL)
+			{
+				free(line);
+				continue;
+			}
+			execute_command(line);
+			free(line);
+		}
+	}
 }
 
 
 
 /**
- * @brief 
- * 
- * @return int 
- */
+* main - main function
+* @argc: arg count
+* @argv: arg vetor
+*
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
-    cmdline_arguments(argc, argv);
-
-    return (0);
-
+	cmdline_arguments(argc, argv);
+	return (0);
 }
+
