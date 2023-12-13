acda740ce465ecdfa6ff7456b75a0cacccd45451
Blessdeb <oyemadedeborah@gmail.com>
2023-12-12 17:06:06 -0800

    Shell.c

--- shell.c
+++ shell.c
@@ -1,30 +1,4 @@
 #include "shell.h"
-<<<<<<< HEAD
-
-/*
- * Author: Deborah & Nonso
- * Main:  main variable
- *
- */
-
-int main(int ac, char **argv)
-{
-	char *prompt = "(simple_shell) $ ";
-	char *lineptr;
-	size_t n = 0;
-
-/* declare void variables */
-
-	(void)ac;
-	(void)argv;
-
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
-
-	free(lineptr);
-	return (0);
-=======
 /**
  * cmdline_arguments - handle cmdline arguments
  * @arc: argument count
@@ -76,5 +50,4 @@ int main(int argc, char *argv[])
 
     return (0);
 
->>>>>>> 4a80745963db0a7ff882c7b5323f88da06ba6a4e
 }
