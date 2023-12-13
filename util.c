5c65a589ac667b39c94295686a77bdbb2847221b
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-12-12 22:27:26 -0800

    util file updated

--- util.c
+++ util.c
@@ -3,7 +3,7 @@
  * string_splitter - splits a string into tokens
  * @string: string to split
  * @separator: string to split by(delimiter)
- * 
+ *
  * Return: split string
  */
 char **string_splitter(char *string, const char *separator)
@@ -11,7 +11,7 @@ char **string_splitter(char *string, const char *separator)
 	static char *buffer;
 	char *tkn, char **tkns;
 	size_t i = 0;
-	
+
 	if (string != NULL)
 		buffer = string;
 	if (buffer == NULL)
@@ -19,7 +19,7 @@ char **string_splitter(char *string, const char *separator)
 	tkns = malloc(sizeof(char *));
 	if (!tkns)
 		return (NULL);
-			
+
 			while ((tkn = strtok(buffer, separator)) != NULL)
 			{
 				buffer = NULL;
@@ -50,7 +50,7 @@ char **string_splitter(char *string, const char *separator)
  * _strcompare - compares two strings
  * @s1: first string
  * @s2: second string
- * 
+ *
  * Return: 0 if equal, 1 if not
  */
 int _strcompare(const char *s1, const char *s2)
@@ -66,13 +66,13 @@ int _strcompare(const char *s1, const char *s2)
 /**
  * _atoint - converts a string to an integer
  * @string: string to convert
- * 
+ *
  * Return: converted integer
  */
 int _atoint(char *string)
 {
 	int output = 0, symb = 1;
-	
+
 	while (*string == ' ' || (*string >= '\t' && *string <= 'r'))
 	{
 		string++;
@@ -82,7 +82,7 @@ int _atoint(char *string)
 		symb = (*string == '-') ? -1 : 1;
 		string++;
 	}
-	
+
 	while (*string >= '0' && *string <= '9')
 	{
 		output = output * 10 + (*string - '0');
