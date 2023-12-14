a8726b9dad0b89a81a19177a92469030196f9770
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2023-12-14 10:16:31 +0100

    a UNIX Command Line Interpreter

--- sh_builtin_cmd2.c
+++ sh_builtin_cmd2.c
@@ -9,10 +9,10 @@ int sh_check_builtin(char **cmd)
 {
 	builtin fun[] = {
 		{"cd", NULL},
-		{"sh_help", NULL},
+		{"help", NULL},
 		{"echo", NULL},
 		{"env", NULL},
-		{"sh_hist", NULL},
+		{"history", NULL},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -41,9 +41,9 @@ int sh_handle_builtins(char **cmd, int st)
 	builtin built_in[] = {
 		{"cd", sh_cd},
 		{"env", dis_env},
-		{"sh_help", display_sh_help},
+		{"help", display_sh_help},
 		{"echo", sh_echo},
-		{"sh_hist", sh_hist_dis},
+		{"history", sh_hist_dis},
 		{NULL, NULL}
 	};
 	int i = 0;
@@ -61,8 +61,8 @@ int sh_handle_builtins(char **cmd, int st)
 
 /**
  * sh_exit - Exit Status for built-in commands
- * @cmd: Array of parsed command strings
- * @entry: Input recieved from user (to be freed)
+ * @cmd: Array of command strings
+ * @entry: Input from user (to be freed)
  * @argv: Arguments before program starts(argv[0] == Shell Program Name)
  * @c: Shell execution count
  * @stat: Exit status
